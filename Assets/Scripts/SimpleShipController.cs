75cd7791675f2bb655243008e6ffa33a85887231
Yannick <yannickbaudraz@gmail.com>
2019-10-25 14:32:29 +0200

    Add Chapter 02.01

---
+++ Assets/Scripts/SimpleShipController.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SimpleShipController : MonoBehaviour
+    {
+        /// <summary>
+        /// This method is called when the script instance is being loaded.
+        /// </summary>
+        [UsedImplicitly]
+        private void Awake()
+        {
+        }
+
+        /// <summary>
+        /// This method is called before the first frame update.
+        /// </summary>
+        [UsedImplicitly]
+        private void Start()
+        {
+        }
+
+        /// <summary>
+        /// This method is called once per frame.
+        /// </summary>
+        [UsedImplicitly]
+        private void Update()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            transform.Rotate(0, 0, -x);
+            transform.Translate(0, y, 0);
+        }
+    }
+}
