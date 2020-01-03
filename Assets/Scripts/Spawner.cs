94feba5cab378a86ed54ead2c0245e717363a0d1
Yannick <yannickbaudraz@gmail.com>
2020-01-03 08:54:09 +0100

    Add Black Hole spawner animation

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -16,14 +16,14 @@ namespace Assets.Scripts
         [Header("SPAWNING")]
         [Range(0.001f, 10f)] public float minRate = 1.0f;
         [Range(0.001f, 10f)] public float maxRate = 1.0f;
-        private float lastRateChanged = 1.0f;
+        private float _lastRateChanged = 1.0f;
         public int number = 5;
         public bool infinite = true;
         private int _remainingObjects;
 
         [Header("LOCATIONS")]
         public GameArea area;
-        private Transform player;
+        private Transform _player;
         public float minDistanceFromPlayer;
 
         [Header("VELOCITY")]
@@ -31,9 +31,23 @@ namespace Assets.Scripts
         [Range(0, 360f)] public float spread = 360f;
         [Range(0, 10)] public float minStrength = 1f;
         [Range(0, 10)] public float maxStrength = 10f;
-        private float lastStrengthChanged = -1;
+        private float _lastStrengthChanged = -1;
+
+        [Header("ANIMATOR")]
+        public string animatorName = "Spawning";
+        public float animatorDelayIn = 1;
+        public float animatorDelayOut = 1;
+        private int _animatorHashId;
+        private Animator _animator;
 
         #endregion
+
+        [UsedImplicitly]
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            if (_animator) _animatorHashId = Animator.StringToHash(animatorName);
+        }
 
         [UsedImplicitly]
         private IEnumerator Start()
@@ -44,17 +58,23 @@ namespace Assets.Scripts
             {
                 GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
                 if (playerGO)
-                    player = playerGO.transform;
+                    _player = playerGO.transform;
                 else
                     Debug.LogWarning("No player found.");
+            }
+
+            if (_animator)
+            {
+                _animator.SetBool(_animatorHashId, true);
+                yield return new WaitForSeconds(animatorDelayIn);
             }
 
             while (infinite || _remainingObjects > 0)
             {
                 Vector3 position = area ? area.GetRandomPosition() : transform.position;
 
-                if (player && Vector3.Distance(position, player.position) < minDistanceFromPlayer)
-                    position = (position - player.position).normalized * minDistanceFromPlayer;
+                if (_player && Vector3.Distance(position, _player.position) < minDistanceFromPlayer)
+                    position = (position - _player.position).normalized * minDistanceFromPlayer;
 
                 GameObject obj = Instantiate(reference, position, transform.rotation);
                 Rigidbody2D rigidBody = obj.GetComponent<Rigidbody2D>();
@@ -74,20 +94,26 @@ namespace Assets.Scripts
 
                 yield return new WaitForSeconds(1 / Random.Range(minRate, maxRate));
             }
+
+            if (_animator)
+            {
+                _animator.SetBool(_animatorHashId, false);
+                yield return new WaitForSeconds(animatorDelayOut);
+            }
         }
 
         [UsedImplicitly]
-        public void OnValidate()
+        private void OnValidate()
         {
-            if (Math.Abs(lastRateChanged - minRate) > 0.01 && minRate > maxRate)
-                lastRateChanged = maxRate = minRate;
-            else if (Math.Abs(lastRateChanged - maxRate) > 0.01 && maxRate < minRate)
-                lastRateChanged = minRate = maxRate;
+            if (Math.Abs(_lastRateChanged - minRate) > 0.01 && minRate > maxRate)
+                _lastRateChanged = maxRate = minRate;
+            else if (Math.Abs(_lastRateChanged - maxRate) > 0.01 && maxRate < minRate)
+                _lastRateChanged = minRate = maxRate;
 
-            if (Math.Abs(lastStrengthChanged - minStrength) > 0.01 && minStrength > maxStrength)
-                lastStrengthChanged = maxStrength = minStrength;
-            else if (Math.Abs(lastStrengthChanged - maxStrength) > 0.01 && maxStrength < minStrength)
-                lastStrengthChanged = minStrength = maxStrength;
+            if (Math.Abs(_lastStrengthChanged - minStrength) > 0.01 && minStrength > maxStrength)
+                _lastStrengthChanged = maxStrength = minStrength;
+            else if (Math.Abs(_lastStrengthChanged - maxStrength) > 0.01 && maxStrength < minStrength)
+                _lastStrengthChanged = minStrength = maxStrength;
         }
     }
 }
