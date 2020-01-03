ff9c35d94691bdbbaaf16d664d1217ae8c2add1d
Yannick <yannickbaudraz@gmail.com>
2020-01-03 01:41:48 +0100

    Add Chapter 04.01

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Spawner : MonoBehaviour
+    {
+        [Header("SPAWN")]
+        public GameObject reference;
+
+        [Header("SPAWNING")]
+        [Range(0.001f, 100f)] public float rate = 1.0f;
+        public int number;
+        public bool infinite;
+
+        private int _remainingObjects;
+
+        [UsedImplicitly]
+        private IEnumerator Start()
+        {
+            _remainingObjects = number;
+
+            while (infinite || _remainingObjects > 0)
+            {
+                Instantiate(reference, transform.position, transform.rotation);
+                _remainingObjects--;
+
+                yield return new WaitForSeconds(1 / rate);
+            }
+        }
+    }
+}
